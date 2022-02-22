1686d897118e889af846a79f40d3db46e1d3ee37
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-02-22 20:27:40 +0200

    Added animation of player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,48 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
-    
+
+    public static bool _flipRight = true;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+      
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            if (movement.x > 0 && !_flipRight)
+            {
+                Flip();
+            }
+            if (movement.x < 0 && _flipRight)
+            {
+                Flip();
+            };
+            animator.SetInteger("Movement", 1);
+        }
+        else
+        {
+            animator.SetInteger("Movement", 0);
+        }
+    }
+
+    public void Flip()
+    {
+        _flipRight = !_flipRight;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
