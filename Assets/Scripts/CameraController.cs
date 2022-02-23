539f8ef423ebc8ab7574a88d653221eac34f87df
DmitryItyl <theodridspm@gmail.com>
2022-02-23 11:43:25 +0200

    Updated camera follow script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform target;
 
+    public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
+    Vector3 minValue, maxValue;
+
+    [SerializeField] Tilemap level;
     // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        Bounds bound = level.localBounds;
+        minValue = bound.min;
+        maxValue = bound.max;
+
+        minValue.x += Camera.main.orthographicSize * Screen.width / Screen.height;
+        minValue.y += Camera.main.orthographicSize;
+
+        maxValue.x -= Camera.main.orthographicSize * Screen.width / Screen.height;
+        maxValue.y -= Camera.main.orthographicSize;
+    }
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 characterPosition = target.position + offset;
+
+        //Keeping the camera within the level bounds
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(characterPosition.x, minValue.x, maxValue.x),
+            Mathf.Clamp(characterPosition.y, minValue.y, maxValue.y),
+            0);
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition + offset;
     }
 }
