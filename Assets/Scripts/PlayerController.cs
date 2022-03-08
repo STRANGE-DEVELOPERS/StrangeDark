005b369120fed5c510e5e007439e9fa8676d3114
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-03-08 13:39:53 +0200

    Improving health after the selection of items

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currentHealth;
     [SerializeField] private HealthBar healthBar;
+    [SerializeField] private GameObject flask, FirstAidKit;
 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     float offset;
 
     Vector2 movement;
+
 
     public static bool _flipRight = true;
     private Animator animator;
@@ -51,8 +53,10 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetInteger("Movement", 0);
         }
-    }
 
+       
+    }
+    
     public void Flip(bool isLookingRight)
     {
         _flipRight = isLookingRight;
@@ -61,11 +65,47 @@ public class PlayerController : MonoBehaviour
         transform.localScale = theScale;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Flask")
+        {
+            IncreaseHealth(30);
+            Destroy(flask);
+        }
+        else if (collision.tag == "FirstAidKit")
+        {
+            currentHealth = maxHealth;
+            healthBar.SetHealth(currentHealth);
+            Destroy(FirstAidKit);
+        }
+
+        else if (collision.tag == "Enemy")
+        {
+            TakeDemage(30);
+        }
+   }
+
     void TakeDemage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
     }
+        
+    void IncreaseHealth(int healt)
+    {
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += healt;
+            if (currentHealth> maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+        }
 
-
+        healthBar.SetHealth(currentHealth);
+    }
 }
