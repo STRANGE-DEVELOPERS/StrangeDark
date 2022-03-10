1909ccee1a3f74dee7dbad0c29ec5164bc7da6e3
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-03-10 21:09:29 +0200

    Random level generation script

---
+++ Assets/Scripts/RoomSpawn.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawn : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+    // 3 --> need left door
+    // 4 --> need right door
+
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+        void Spawn()
+    {
+        if (spawned == false)
+        {
+           // if (openingDirection == 1)
+           // {
+           //     // Need to spawn a room with a BOTTOM door.
+           //     rand = Random.Range(0, templates.bottonRooms.Length);
+           //     Instantiate(templates.bottonRooms[rand], transform.position, templates.bottonRooms[rand].transform.rotation);
+           // }
+           //else
+           if (openingDirection == 2)
+            {
+                // Need to spawn a room with a TOP door.
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                //Need to spawn a room with a LEFT door.
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            //else if (openingDirection == 4)
+            //{
+            //    // Need to spawn a room with a RIGHT door.
+            //    rand = Random.Range(0, templates.rightRooms.Length);
+            //    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            //}
+            spawned = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawn>().spawned)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    //public GameObject[] bottonRooms;
+    public GameObject[] leftRooms;
+    //public GameObject[] rightRooms;
+    public GameObject[] topRooms;
+}
