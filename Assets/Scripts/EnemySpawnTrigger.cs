929ca90760e4a16b90fcfc653048fb9a178dbe61
AndriiHensiruk <Hensiruk@gmail.com>
2022-03-11 18:17:18 +0200

    Creat Enemy Spavner

---
+++ Assets/Scripts/EnemySpawnTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private Transform[] EnemySpawnerPosition;
+    private int _randomSpawnPointts;
+    public float RepeatRate = 3f;
+    public int DestroySpawner = 20;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            InvokeRepeating("EnemySpawner", 1f, RepeatRate);
+            Destroy(gameObject, DestroySpawner);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private void EnemySpawner()
+    {
+        _randomSpawnPointts = Random.Range(0, EnemySpawnerPosition.Length);
+        Instantiate(Enemy, EnemySpawnerPosition[_randomSpawnPointts].position, Quaternion.identity);
+    }
+
+
+
+}
