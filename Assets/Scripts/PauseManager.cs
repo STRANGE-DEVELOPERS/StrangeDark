c47f37a508a891bd02666ce86cccf71e19581b69
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-03-17 20:46:04 +0200

    Create a main menu and pause button

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseManager : MonoBehaviour
+{
+    public GameObject [] pauseMenuUI;
+    public static bool GameIsPause = false;
+    public static int numberScen=4;
+   
+    public void StartPause()
+    {
+         if (GameIsPause)
+            {
+               Restart();
+            }
+            else
+            {
+                Pause();
+            }
+    }
+
+    public void ButtonMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        PlayerPrefs.SetInt("PositionPlayer", 0);
+        numberScen =  SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+
+    void Pause()
+    {
+        for (int i=0; i<3; i++)
+        {
+            pauseMenuUI[i].SetActive(true);
+        };
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    void Restart()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            pauseMenuUI[i].SetActive(false);
+        };
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+}
